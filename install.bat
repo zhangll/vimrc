de9278528e4deaa2543b7993d5d5a967e9ca95f3
lele.zhang <lele.zhang@servision.com.cn>
2016-05-05 18:42:08 +0800

    add idea vim

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 mklink  .vimrc .vim\vimrc
 mklink  .vrapperrc .vim\vrapper\_vrapperrc
+mklink  .ideavimrc  .vim\ideavim\_ideavimrc
 
 set path=%path%;C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\Vim\vim74
 
